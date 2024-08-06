957a1a54f5e16ead91667738f5a44a510062e7fb
Franklin <franklinpereira98@hotmail.com>
2024-08-06 18:35:12 -0300

    Inital Commit

---
+++ src/main/kotlin/com/hyuse/nikkeManager/NikkeManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.hyuse.nikkeManager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NikkeManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<NikkeManagerApplication>(*args)
+}
