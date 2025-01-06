9bb0e9cd8f7dee454d3299b96be48ff8d7263ef9
Franklin <franklinpereira98@hotmail.com>
2025-01-06 17:47:15 -0300

    Implement test for toModel function

--- src/test/kotlin/com/hyuse/nikkeManager/dto/NikkeDTOTest.kt
+++ src/test/kotlin/com/hyuse/nikkeManager/dto/NikkeDTOTest.kt
@@ -3,6 +3,7 @@ package com.hyuse.nikkeManager.dto
 import com.hyuse.nikkeManager.enums.*
 import jakarta.validation.Validation
 import jakarta.validation.Validator
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -37,6 +38,34 @@ class NikkeDTOTest {
         val violations = validator.validate(nikkeDTO)
 
         assertTrue(violations.isEmpty())
+    }
+
+    @Test
+    @DisplayName("Should Test if converting correctly a DTO to a Model")
+    fun toModelTest() {
+
+        val nikkeDTO = NikkeDTO(
+            id = null,
+            name = "Test1",
+            core = 1,
+            attraction = 1,
+            skill1Level = 1,
+            skill2Level = 1,
+            burstLevel = 1,
+            rarity = Rarity.SSR,
+            ownedStatus = OwnedStatus.NOT_OWNED,
+            burstType = BurstType.III,
+            company = Company.PILGRIM,
+            code = Code.ELECTRIC,
+            weapon = Weapon.SR,
+            nikkeClass = NikkeClass.ATTACKER,
+            cube = null,
+            doll = null
+        )
+
+        val nikkeModel = nikkeDTO.toModel()
+
+        assertThat(nikkeModel.name).isEqualTo(nikkeDTO.name)
     }
 
 }
