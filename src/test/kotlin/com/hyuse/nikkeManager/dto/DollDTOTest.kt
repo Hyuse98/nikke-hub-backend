744093eb91bcb9141d872467b66d45755408d63f
Franklin <franklinpereira98@hotmail.com>
2025-01-07 15:01:45 -0300

    Implementing Doll test where invalidate DTO when is level is above range

--- src/test/kotlin/com/hyuse/nikkeManager/dto/DollDTOTest.kt
+++ src/test/kotlin/com/hyuse/nikkeManager/dto/DollDTOTest.kt
@@ -3,6 +3,7 @@ package com.hyuse.nikkeManager.dto
 import com.hyuse.nikkeManager.enums.Rarity
 import jakarta.validation.Validation
 import jakarta.validation.Validator
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -24,6 +25,24 @@ class DollDTOTest {
         val violations = validator.validate(dollDTO)
 
         assertTrue(violations.isEmpty())
+    }
+
+    @Test
+    @DisplayName("Should fail validation when level is above 15")
+    fun invalidDollDTOCase1() {
+        // Given
+        val dollDTO = DollDTO(
+            id = 1,
+            rarity = Rarity.SSR,
+            level = 16
+        )
+
+        // When
+        val violations = validator.validate(dollDTO)
+
+        // Then
+        assertFalse(violations.isEmpty())
+        assertTrue(violations.any { it.message == "Cant be more than 15" })
     }
 
 }
