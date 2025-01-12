2b8fdb53efec3e89b0c626984abe5e8ba091c300
Franklin <franklinpereira98@hotmail.com>
2025-01-12 14:26:04 -0300

    little refactor

--- src/main/kotlin/com/hyuse/nikkeManager/repository/DollRepository.kt
+++ src/main/kotlin/com/hyuse/nikkeManager/repository/DollRepository.kt
@@ -3,8 +3,7 @@ package com.hyuse.nikkeManager.repository
 import com.hyuse.nikkeManager.enums.Rarity
 import com.hyuse.nikkeManager.model.Doll
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.Optional
 
 interface DollRepository : JpaRepository<Doll, Int> {
-    fun findByRarityAndLevel(rarity: Rarity, level: Int): Optional<Doll>
+    fun findByRarityAndLevel(rarity: Rarity, level: Int): Doll?
 }
