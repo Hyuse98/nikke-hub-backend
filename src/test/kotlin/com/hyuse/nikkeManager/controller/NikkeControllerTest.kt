39a4b944bf2d0acd063a221b10978bd61fe3faf6
Franklin <franklinpereira98@hotmail.com>
2025-01-13 13:03:34 -0300

    implemented NikkeControllerTest to createNikke when is success and return http 201

---
+++ src/test/kotlin/com/hyuse/nikkeManager/controller/NikkeControllerTest.kt
@@ -0,0 +1,83 @@
+package com.hyuse.nikkeManager.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.hyuse.nikkeManager.dto.NikkeDTO
+import com.hyuse.nikkeManager.enums.*
+import com.hyuse.nikkeManager.repository.NikkeRepository
+import com.hyuse.nikkeManager.service.NikkeService
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@WebMvcTest(NikkeController::class)
+class NikkeControllerTest {
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @MockkBean
+    lateinit var nikkeService: NikkeService
+
+    @MockkBean
+    lateinit var nikkeRepository: NikkeRepository
+
+    @Test
+    @DisplayName("201")
+    fun createNikkeCase1() {
+
+        val request = NikkeDTO(
+            id = null,
+            name = "Test1",
+            core = 1,
+            attraction = 1,
+            skill1Level = 1,
+            skill2Level = 1,
+            burstLevel = 1,
+            rarity = Rarity.SR,
+            ownedStatus = OwnedStatus.NOT_OWNED,
+            burstType = BurstType.III,
+            company = Company.PILGRIM,
+            code = Code.ELECTRIC,
+            weapon = Weapon.MG,
+            nikkeClass = NikkeClass.SUPPORTER,
+            cube = null,
+            doll = null
+        )
+
+        val response = request.copy(id = 1)
+
+        every { nikkeService.createNikke(request) } returns response.toModel()
+
+        mockMvc.perform(
+            post("/nikke")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ObjectMapper().writeValueAsString(request))
+        )
+            .andExpect(status().isCreated)
+            .andExpect(jsonPath("$.id").value(1))
+            .andExpect(jsonPath("$.name").value("Test1"))
+            .andExpect(jsonPath("$.core").value(1))
+            .andExpect(jsonPath("$.attraction").value(1))
+            .andExpect(jsonPath("$.skill1Level").value(1))
+            .andExpect(jsonPath("$.skill2Level").value(1))
+            .andExpect(jsonPath("$.burstLevel").value(1))
+            .andExpect(jsonPath("$.rarity").value(Rarity.SR.name))
+            .andExpect(jsonPath("$.ownedStatus").value(OwnedStatus.NOT_OWNED.name))
+            .andExpect(jsonPath("$.burstType").value(BurstType.III.name))
+            .andExpect(jsonPath("$.company").value(Company.PILGRIM.name))
+            .andExpect(jsonPath("$.code").value(Code.ELECTRIC.name))
+            .andExpect(jsonPath("$.weapon").value(Weapon.MG.name))
+            .andExpect(jsonPath("$.nikkeClass").value(NikkeClass.SUPPORTER.name))
+            .andExpect(jsonPath("$.cube").doesNotExist())
+            .andExpect(jsonPath("$.doll").doesNotExist())
+    }
+
+}
