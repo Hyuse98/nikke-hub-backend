c0b35b00260014737c1988dcf5235a374dfc4d3f
Franklin <franklinpereira98@hotmail.com>
2025-01-16 12:48:30 -0300

    Add New Character

---
+++ src/main/resources/db/migration/V20__Insert_New_Character_Missilis_Mana.sql
@@ -0,0 +1,2 @@
+INSERT INTO nikke (id, name, core, attraction, skill1Level, skill2Level, burst_level, rarity, owned_status, burst_type, company, code, weapon, nikke_class, cube, doll_id)
+VALUES (143, 'Mana', 0, 1, 1, 1, 1, 'SSR', 'NOT_OWNED', 'III', 'MISSILIS', 'WIND', 'AR', 'ATTACKER', null, null);
