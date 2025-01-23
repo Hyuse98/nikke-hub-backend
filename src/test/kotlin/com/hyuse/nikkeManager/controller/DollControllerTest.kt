1bdb7feb292f3568544747c674ec6c264003bac5
Franklin <franklinpereira98@hotmail.com>
2025-01-23 09:57:45 -0300

    Add Doll Controller Test where list a doll is success

--- src/test/kotlin/com/hyuse/nikkeManager/controller/DollControllerTest.kt
+++ src/test/kotlin/com/hyuse/nikkeManager/controller/DollControllerTest.kt
@@ -80,5 +80,40 @@ class DollControllerTest {
             .andExpect(jsonPath("$.message").value("Doll Rarity: '${request.rarity}' Level: '${request.level}' already exists"))
     }
 
+    @Test
+    @DisplayName("200")
+    fun listDollsCase1() {
+
+        val doll1 = Doll(
+            id = 1,
+            rarity = Rarity.SSR,
+            level = 5
+        )
+
+        val doll2 = Doll(
+            id = 2,
+            rarity = Rarity.SR,
+            level = 15
+        )
+
+        val expectDolls = listOf(
+            doll1, doll2
+        )
+
+        every { dollService.listDolls() } returns expectDolls
+
+        mockMvc.perform(
+            get("/doll")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$", hasSize<Any>(2)))
+            .andExpect(jsonPath("$[0].rarity").value(doll1.rarity.name))
+            .andExpect(jsonPath("$[0].level").value(doll1.level))
+            .andExpect(jsonPath("$[1].rarity").value(doll2.rarity.name))
+            .andExpect(jsonPath("$[1].level").value(doll2.level))
+    }
+
+
 
 }
