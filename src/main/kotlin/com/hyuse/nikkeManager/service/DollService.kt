360ba5156bec8839a29b1cb02a65149e0356c4fd
Franklin <franklinpereira98@hotmail.com>
2025-01-28 09:12:54 -0300

    Add deleteById function on DollService

--- src/main/kotlin/com/hyuse/nikkeManager/service/DollService.kt
+++ src/main/kotlin/com/hyuse/nikkeManager/service/DollService.kt
@@ -3,6 +3,7 @@ package com.hyuse.nikkeManager.service
 import com.hyuse.nikkeManager.dto.DollDTO
 import com.hyuse.nikkeManager.enums.Rarity
 import com.hyuse.nikkeManager.exception.DollAlreadyExistsException
+import com.hyuse.nikkeManager.exception.DollNotFoundException
 import com.hyuse.nikkeManager.model.Doll
 import com.hyuse.nikkeManager.repository.DollRepository
 import jakarta.transaction.Transactional
@@ -19,11 +20,17 @@ class DollService(val dollRepository: DollRepository) {
         return dollRepository.save(dollDTO.toModel())
     }
 
-    fun listDolls(): List<Doll>{
+    fun listDolls(): List<Doll> {
         return dollRepository.findAll();
     }
 
-    fun searchDoll(rarity: Rarity, level: Int): Doll?{
+    fun searchDoll(rarity: Rarity, level: Int): Doll? {
         return dollRepository.findByRarityAndLevel(rarity, level)
     }
+
+    fun deleteDollById(id: Int) {
+        val doll = dollRepository.findById(id).orElseThrow { DollNotFoundException(id) }
+        dollRepository.deleteById(id)
+    }
+
 }
