fd6a88a3d88abf8385a7e9229b88dd3ffd00a126
Franklin <franklinpereira98@hotmail.com>
2025-02-19 20:40:32 -0300

    Some lines change, nothing important

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM eclipse-temurin:17.0.14_7-jre-jammy
 
-#FROM openjdk:17-jdk-slim
-
 WORKDIR /app
 
 COPY build/libs/Nikke-Hub-Backend-0.0.1-SNAPSHOT.jar /app/app.jar
--- src/main/kotlin/com/hyuse/nikkeManager/NikkeManagerApplication.kt
+++ src/main/kotlin/com/hyuse/nikkeManager/NikkeManagerApplication.kt
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @OpenAPIDefinition(info = Info(title = "Nikke Manager", version = "Beta", description = "API to manage Nikkes"))
 @SpringBootApplication
