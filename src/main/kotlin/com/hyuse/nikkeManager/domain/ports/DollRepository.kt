7bde6734b80e822ec0c4a62ed0c06e1e2f46ffe7
Franklin <franklinpereira98@hotmail.com>
2025-06-04 12:05:26 -0300

    Created Domain Interface Ports

---
+++ src/main/kotlin/com/hyuse/nikkeManager/domain/ports/DollRepository.kt
@@ -0,0 +1,26 @@
+package com.hyuse.nikkeManager.domain.ports
+
+import com.hyuse.nikkeManager.domain.entities.Doll
+
+interface DollRepository {
+
+    fun save(doll: Doll): Doll
+    fun update(id: Int, doll: Doll): Doll
+    fun delete(id: Int): Boolean
+
+    fun findById(id: Int): Doll
+//    fun findByRarity(rarity: Rarity): Optional<Doll>
+//    fun findByLevel(level: Int): Doll
+
+    fun findAll(): Collection<Doll>
+
+    //TODO Extra use cases
+    /*
+    * existByRarity
+    * existByLevel
+    *
+    */
+
+    fun existById(id: Int): Boolean
+
+}
