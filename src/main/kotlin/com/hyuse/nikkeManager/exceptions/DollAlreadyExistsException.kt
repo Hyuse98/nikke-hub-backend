b565ea1c8c339ae35364a0de807c484edf15a927
Franklin <franklinpereira98@hotmail.com>
2025-06-07 12:10:47 -0300

    Deleted File

--- src/main/kotlin/com/hyuse/nikkeManager/exceptions/DollAlreadyExistsException.kt
+++
@@ -1,5 +0,0 @@
-package com.hyuse.nikkeManager.exceptions
-
-import com.hyuse.nikkeManager.enums.Rarity
-
-class DollAlreadyExistsException(rarity: Rarity, level: Int): NikkeException("Doll Rarity: '$rarity' Level: '$level' already exists")
