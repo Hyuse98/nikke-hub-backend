a7a451dcc94f950a252162c492350da2669df601
Franklin <franklinpereira98@hotmail.com>
2025-06-07 16:57:01 -0300

    Refactoring Methods on Nikke Controller

--- src/main/kotlin/com/hyuse/nikkeManager/infrastructure/web/controller/NikkeController.kt
+++ src/main/kotlin/com/hyuse/nikkeManager/infrastructure/web/controller/NikkeController.kt
@@ -2,18 +2,14 @@ package com.hyuse.nikkeManager.infrastructure.web.controller
 
 
 import com.hyuse.nikkeManager.domain.entities.Nikke
-import com.hyuse.nikkeManager.domain.enums.*
-import com.hyuse.nikkeManager.domain.usecases.nikke.CreateNikkeCase
+import com.hyuse.nikkeManager.domain.usecases.nikke.*
 import com.hyuse.nikkeManager.infrastructure.web.dto.NikkeDTO
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.Valid
-import org.springdoc.core.annotations.ParameterObject
-import org.springframework.data.domain.Pageable
 import org.springframework.hateoas.EntityModel
-import org.springframework.hateoas.PagedModel
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn
 import org.springframework.http.HttpStatus
@@ -25,7 +21,11 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody as SwaggerRequestBod
 @RestController
 @RequestMapping("/api/nikke")
 class NikkeController(
-    private val createNikkeCase: CreateNikkeCase
+    private val createNikkeCase: CreateNikkeCase,
+    private val updateNikkeCase: UpdateNikkeCase,
+    private val deleteNikkeCase: DeleteNikkeCase,
+    private val getNikkeByNameCase: GetNikkeByNameCase,
+    private val getAllNikkesCase: GetAllNikkesCase
 ) {
 
     @Operation(
@@ -87,7 +87,7 @@ class NikkeController(
         val entityModel = EntityModel.of(
             nikke,
             linkTo(methodOn(this::class.java).getNikke(nikke.name)).withSelfRel(),
-            linkTo(methodOn(this::class.java).listAllNikke(pageable = Pageable.unpaged())).withRel("Collection")
+            linkTo(methodOn(this::class.java).listAllNikke()).withRel("Collection")
         )
 
         return ResponseEntity.status(HttpStatus.CREATED).body(entityModel)
@@ -105,11 +105,19 @@ class NikkeController(
                 mediaType = "application/json",
                 schema = Schema(
                     example = """{
+                    "name": "string",
                     "core": 0,
                     "attraction": 0,
                     "skill1Level": 0,
                     "skill2Level": 0,
                     "burstLevel": 0,
+                    "rarity": "R",
+                    "ownedStatus": "NOT_OWNED",
+                    "burstType": "I",
+                    "company": "ELYSION",
+                    "code": "FIRE",
+                    "weapon": "AR",
+                    "nikkeClass": "ATTACKER",
                     "cube": "ASSAULT",
                     "doll": {
                         "id": 0
@@ -119,12 +127,30 @@ class NikkeController(
             )]
         )
     )
-    @PutMapping("/{name}")
+    @PutMapping("/{id}")
     fun updateNikke(
         @RequestBody @Valid nikkeDTO: NikkeDTO,
-        @PathVariable name: String
+        @PathVariable id: Int
     ): ResponseEntity<Nikke> {
-        return ResponseEntity.status(HttpStatus.CREATED).body(nikkeService.updateNikke(nikkeDTO, name))
+
+        val useCaseInput = UpdateNikkeCase.Input(
+            name = nikkeDTO.name,
+            core = nikkeDTO.core,
+            attraction = nikkeDTO.attraction,
+            skill1 = nikkeDTO.skill1,
+            skill2 = nikkeDTO.skill2,
+            skillBurst = nikkeDTO.skillBurst,
+            rarity = nikkeDTO.rarity,
+            ownedStatus = nikkeDTO.ownedStatus,
+            burstType = nikkeDTO.burstType,
+            company = nikkeDTO.company,
+            code = nikkeDTO.code,
+            weapon = nikkeDTO.weapon,
+            nikkeClass = nikkeDTO.nikkeClass,
+            cube = nikkeDTO.cube
+        )
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(updateNikkeCase.execute(id, useCaseInput))
     }
 
     @Operation(
@@ -133,52 +159,52 @@ class NikkeController(
         description = "This endpoint delete a nikke by his name on path variable",
         tags = ["Nikke"],
     )
-    @DeleteMapping("/{name}")
-    fun deleteNikke(@PathVariable name: String) {
-        return nikkeService.deleteNikke(name)
+    @DeleteMapping("/{id}")
+    fun deleteNikke(@PathVariable id: Int) {
+        return deleteNikkeCase.execute(id)
     }
 
-    @Operation(
-        method = "GET",
-        summary = "List all Nikkes Filtered",
-        description = "This endpoint will return a list of all nikkes available on database with filters applied",
-        tags = ["Nikke"],
-    )
-    @GetMapping("/filtered")
-    fun listAllNikkeFiltered(
-        @RequestParam(required = false) rarity: Rarity?,
-        @RequestParam(required = false) ownedStatus: OwnedStatus?,
-        @RequestParam(required = false) burstType: BurstType?,
-        @RequestParam(required = false) company: Company?,
-        @RequestParam(required = false) code: Code?,
-        @RequestParam(required = false) weapon: Weapon?,
-        @RequestParam(required = false) nikkeClass: NikkeClass?,
-        @RequestParam(required = false) cube: Cubes?,
-        @ParameterObject pageable: Pageable
-    ): ResponseEntity<PagedModel<EntityModel<Nikke>>> {
-
-        val nikkesPage = nikkeService.listAllNikkeFiltered(
-            rarity, ownedStatus, burstType, company, code, weapon, nikkeClass, cube, pageable
-        )
-
-        val nikkes = nikkesPage.content.map { nikke ->
-            EntityModel.of(
-                nikke,
-                linkTo(methodOn(this::class.java).getNikke(nikke.name)).withSelfRel()
-            )
-        }
-
-        val pagedModel = PagedModel.of(
-            nikkes,
-            PagedModel.PageMetadata(
-                nikkesPage.size.toLong(),
-                nikkesPage.number.toLong(),
-                nikkesPage.totalElements,
-                nikkesPage.totalPages.toLong()
-            )
-        )
-        return ResponseEntity.ok(pagedModel)
-    }
+//    @Operation(
+//        method = "GET",
+//        summary = "List all Nikkes Filtered",
+//        description = "This endpoint will return a list of all nikkes available on database with filters applied",
+//        tags = ["Nikke"],
+//    )
+//    @GetMapping("/filtered")
+//    fun listAllNikkeFiltered(
+//        @RequestParam(required = false) rarity: Rarity?,
+//        @RequestParam(required = false) ownedStatus: OwnedStatus?,
+//        @RequestParam(required = false) burstType: BurstType?,
+//        @RequestParam(required = false) company: Company?,
+//        @RequestParam(required = false) code: Code?,
+//        @RequestParam(required = false) weapon: Weapon?,
+//        @RequestParam(required = false) nikkeClass: NikkeClass?,
+//        @RequestParam(required = false) cube: Cubes?,
+//        @ParameterObject pageable: Pageable
+//    ): ResponseEntity<PagedModel<EntityModel<Nikke>>> {
+//
+//        val nikkesPage = nikkeService.listAllNikkeFiltered(
+//            rarity, ownedStatus, burstType, company, code, weapon, nikkeClass, cube, pageable
+//        )
+//
+//        val nikkes = nikkesPage.content.map { nikke ->
+//            EntityModel.of(
+//                nikke,
+//                linkTo(methodOn(this::class.java).getNikke(nikke.name)).withSelfRel()
+//            )
+//        }
+//
+//        val pagedModel = PagedModel.of(
+//            nikkes,
+//            PagedModel.PageMetadata(
+//                nikkesPage.size.toLong(),
+//                nikkesPage.number.toLong(),
+//                nikkesPage.totalElements,
+//                nikkesPage.totalPages.toLong()
+//            )
+//        )
+//        return ResponseEntity.ok(pagedModel)
+//    }
 
     @Operation(
         method = "GET",
@@ -187,28 +213,11 @@ class NikkeController(
         tags = ["Nikke"],
     )
     @GetMapping
-    fun listAllNikke(@ParameterObject pageable: Pageable): ResponseEntity<PagedModel<EntityModel<Nikke>>> {
+    fun listAllNikke(): ResponseEntity<Collection<Nikke>> {
 
-        val nikkesPage = nikkeService.listAllNikke(pageable)
+        val nikkes = getAllNikkesCase.execute()
 
-        val nikkes = nikkesPage.content.map { nikke ->
-            EntityModel.of(
-                nikke,
-                linkTo(methodOn(this::class.java).getNikke(nikke.name)).withSelfRel()
-            )
-        }
-
-        val pagedModel = PagedModel.of(
-            nikkes,
-            PagedModel.PageMetadata(
-                nikkesPage.size.toLong(),
-                nikkesPage.number.toLong(),
-                nikkesPage.totalElements,
-                nikkesPage.totalPages.toLong()
-            ),
-            linkTo(methodOn(this::class.java).listAllNikke(pageable)).withSelfRel()
-        )
-        return ResponseEntity.ok(pagedModel)
+        return ResponseEntity.ok(nikkes)
     }
 
     @Operation(
@@ -220,10 +229,10 @@ class NikkeController(
     @GetMapping("/{name}")
     fun getNikke(@Parameter(description = "Name of nikke to be search") @PathVariable name: String): ResponseEntity<EntityModel<Nikke>> {
 
-        val nikke = nikkeService.searchNikke(name)
+        val nikke = getNikkeByNameCase.execute(name)
         val entityModel = EntityModel.of(
-            nikke!!,
-            linkTo(methodOn(this::class.java).listAllNikke(pageable = Pageable.unpaged())).withRel("Collection")
+            nikke,
+            linkTo(methodOn(this::class.java).listAllNikke()).withRel("Collection")
         )
         return ResponseEntity.ok(entityModel)
     }
