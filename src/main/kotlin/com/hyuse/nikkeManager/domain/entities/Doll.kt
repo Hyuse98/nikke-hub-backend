6a36c9ce2df3847fd5c2e90d9a22993d2dbbfb85
Franklin <franklinpereira98@hotmail.com>
2025-06-13 10:02:09 -0300

    Refactoring Doll do Rich Domain Model

--- src/main/kotlin/com/hyuse/nikkeManager/domain/entities/Doll.kt
+++ src/main/kotlin/com/hyuse/nikkeManager/domain/entities/Doll.kt
@@ -2,11 +2,46 @@ package com.hyuse.nikkeManager.domain.entities
 
 import com.hyuse.nikkeManager.domain.enums.Rarity
 
-data class Doll(
-
+class Doll private constructor(
     val id: Int? = null,
-    val rarity: Rarity,
-    val level: Int
+    var rarity: Rarity,
+    var level: Int
 ) {
 
+    init {
+        validate()
+    }
+
+    fun updateInfo(newRarity: Rarity, newLevel: Int) {
+        this.rarity = newRarity
+        this.level = newLevel
+        validate()
+    }
+
+    private fun validate() {
+        if (level < MIN_LEVEL || level > MAX_LEVEL) {
+            throw IllegalArgumentException("Doll level must be between $MIN_LEVEL and $MAX_LEVEL.")
+        }
+
+    }
+
+    companion object {
+        private const val MIN_LEVEL = 0
+        private const val MAX_LEVEL = 15
+
+        fun create(rarity: Rarity, level: Int): Doll {
+            return Doll(
+                rarity = rarity,
+                level = level
+            )
+        }
+
+        fun withId(id: Int, rarity: Rarity, level: Int): Doll {
+            return Doll(
+                id = id,
+                rarity = rarity,
+                level = level
+            )
+        }
+    }
 }
