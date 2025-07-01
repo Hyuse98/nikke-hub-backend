048dcf37c5dabf804f8d8414991bf70577aa0494
Franklin <franklinpereira98@hotmail.com>
2025-07-01 16:35:27 -0300

    Update Nikke Attributes to VOs

--- src/main/kotlin/com/hyuse/nikkeManager/domain/entities/Nikke.kt
+++ src/main/kotlin/com/hyuse/nikkeManager/domain/entities/Nikke.kt
@@ -1,16 +1,20 @@
 package com.hyuse.nikkeManager.domain.entities
 
 import com.hyuse.nikkeManager.domain.enums.*
+import com.hyuse.nikkeManager.domain.vo.AttractionLevel
+import com.hyuse.nikkeManager.domain.vo.CharacterName
+import com.hyuse.nikkeManager.domain.vo.CoreLevel
+import com.hyuse.nikkeManager.domain.vo.SkillLevel
 
 class Nikke private constructor(
 
     val id: Int? = null,
-    var name: String,
-    var core: Int,
-    var attraction: Int,
-    var skill1: Int,
-    var skill2: Int,
-    var skillBurst: Int,
+    var name: CharacterName,
+    var core: CoreLevel,
+    var attraction: AttractionLevel,
+    var skill1: SkillLevel,
+    var skill2: SkillLevel,
+    var skillBurst: SkillLevel,
     var rarity: Rarity,
     var ownedStatus: OwnedStatus,
     var burstType: BurstType,
@@ -27,7 +31,7 @@ class Nikke private constructor(
     }
 
     fun correctBaseData(
-        name: String,
+        name: CharacterName,
         rarity: Rarity,
         burstType: BurstType,
         company: Company,
@@ -42,92 +46,56 @@ class Nikke private constructor(
         this.code = code
         this.weapon = weapon
         this.nikkeClass = nikkeClass
-        validate()
         return this
     }
 
     fun skill1LevelUp(): Nikke {
-        this.skill1 += 1
-        validate()
+        this.skill1 = skill1.levelUp()
         return this
     }
 
     fun skill2LevelUp(): Nikke {
-        this.skill2 += 1
-        validate()
+        this.skill2 = skill2.levelUp()
         return this
     }
 
     fun skillBurstLevelUp(): Nikke {
-        this.skillBurst += 1
-        validate()
-        return this
-    }
-
-    fun attractionIncrement(): Nikke {
-        this.attraction += 1
-        validate()
+        this.skillBurst = skillBurst.levelUp()
         return this
     }
 
     fun coreLevelUp(): Nikke {
-        this.core += 1
-        validate()
+        this.core = core.levelUp()
+        return this
+    }
+
+    fun attractionIncrement(): Nikke {
+        this.attraction = attraction.increment()
         return this
     }
 
     fun updateOwnedStatus(ownedStatus: OwnedStatus): Nikke {
         this.ownedStatus = ownedStatus
-        validate()
         return this
     }
 
     fun updateCube(cubes: Cubes): Nikke {
         this.cube = cubes
-        validate()
         return this
     }
 
     private fun validate() {
-
-        if (core < CORE_MIN_LEVEL || core > CORE_MAX_LEVEL) {
-            throw IllegalArgumentException("Core must be Higher than $CORE_MIN_LEVEL and Lower than $CORE_MAX_LEVEL")
-        }
-
-        if (attraction < ATTRACTION_MIN_LEVEL || attraction > ATTRACTION_MAX_LEVEL) {
-            throw IllegalArgumentException("Attraction must be Higher than $ATTRACTION_MIN_LEVEL and Lower than $ATTRACTION_MAX_LEVEL")
-        }
-
-        if (skill1 < SKILL_MIN_LEVEL || skill1 > SKILL_MAX_LEVEL) {
-            throw IllegalArgumentException("Skill 1 must be Higher than $SKILL_MIN_LEVEL and Lower than $SKILL_MAX_LEVEL")
-        }
-
-        if (skill2 < SKILL_MIN_LEVEL || skill2 > SKILL_MAX_LEVEL) {
-            throw IllegalArgumentException("Skill 2 must be Higher than $SKILL_MIN_LEVEL and Lower than $SKILL_MAX_LEVEL")
-        }
-
-        if (skillBurst < SKILL_MIN_LEVEL || skillBurst > SKILL_MAX_LEVEL) {
-            throw IllegalArgumentException("Skill Burst must be Higher than $SKILL_MIN_LEVEL and Lower than $SKILL_MAX_LEVEL")
-        }
     }
 
     companion object {
 
-        private const val CORE_MIN_LEVEL = 0
-        private const val CORE_MAX_LEVEL = 10
-        private const val SKILL_MIN_LEVEL = 1
-        private const val SKILL_MAX_LEVEL = 10
-        private const val ATTRACTION_MIN_LEVEL = 1
-        private const val ATTRACTION_MAX_LEVEL = 30
-
-
         fun create(
-            name: String,
-            core: Int,
-            attraction: Int,
-            skill1: Int,
-            skill2: Int,
-            skillBurst: Int,
+            name: CharacterName,
+            core: CoreLevel,
+            attraction: AttractionLevel,
+            skill1: SkillLevel,
+            skill2: SkillLevel,
+            skillBurst: SkillLevel,
             rarity: Rarity,
             ownedStatus: OwnedStatus,
             burstType: BurstType,
@@ -153,22 +121,25 @@ class Nikke private constructor(
             )
         }
 
-        fun createWhitId(
-            name: String,
-            core: Int,
-            attraction: Int,
-            skill1: Int,
-            skill2: Int,
-            skillBurst: Int,
+        fun reconstitute(
+            id: Int?,
+            name: CharacterName,
+            core: CoreLevel,
+            attraction: AttractionLevel,
+            skill1: SkillLevel,
+            skill2: SkillLevel,
+            skillBurst: SkillLevel,
             rarity: Rarity,
             ownedStatus: OwnedStatus,
             burstType: BurstType,
             company: Company,
             code: Code,
             weapon: Weapon,
-            nikkeClass: NikkeClass
+            nikkeClass: NikkeClass,
+            cube: Cubes?
         ): Nikke {
             return Nikke(
+                id = id,
                 name = name,
                 core = core,
                 attraction = attraction,
@@ -181,7 +152,8 @@ class Nikke private constructor(
                 company = company,
                 code = code,
                 weapon = weapon,
-                nikkeClass = nikkeClass
+                nikkeClass = nikkeClass,
+                cube = cube
             )
         }
     }
