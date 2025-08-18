f85414170447698b4bbc764a28ffc8114a2fdb00
Franklin <franklinpereira98@hotmail.com>
2025-08-18 14:05:42 -0300

    Fixing Repository Method Signature

--- src/main/kotlin/com/hyuse/nikkeManager/infrastructure/database/jpa/repository/doll/DollJpaRepository.kt
+++ src/main/kotlin/com/hyuse/nikkeManager/infrastructure/database/jpa/repository/doll/DollJpaRepository.kt
@@ -1,13 +1,16 @@
 package com.hyuse.nikkeManager.infrastructure.database.jpa.repository.doll
 
+import com.hyuse.nikkeManager.domain.enums.Rarity
+import com.hyuse.nikkeManager.domain.vo.DollLevel
 import com.hyuse.nikkeManager.infrastructure.database.jpa.entity.DollJpaEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface DollJpaRepository : JpaRepository<DollJpaEntity, Int> {
 
     fun save(doll: DollJpaEntity): DollJpaEntity
 
-    fun findByRarityAndLevel(doll: DollJpaEntity): MutableList<DollJpaEntity>
+    fun findByRarityAndLevel(rarity: Rarity, level: DollLevel): Optional<DollJpaEntity>
 }
